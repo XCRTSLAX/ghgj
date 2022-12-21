96133f612d34995d7436097132aae6838b1c3736
XCRTSLAX <100578924+XCRTSLAX@users.noreply.github.com>
2022-12-21 07:31:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://github.com/KR-BOTZ/rent-bot3 rent-bot3 && cd rent-bot3 && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://github.com/XCRTSLAX/opopo opopo && cd opopo && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
